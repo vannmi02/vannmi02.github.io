f51c8f68bdd641b873601fd0e4b91245a0befd4f
vannmi02 <vannmi02@luther.edu>
2015-10-22 23:47:55 -0500

    created millerEdit.js

---
+++ millerEdit.js
@@ -0,0 +1,40 @@
+localSave = function(saveList) {
+    res = []
+    var i;
+    allEntries = document.querySelectorAll('#'+saveList+' li')
+    for(i=0; i < allEntries.length; i++) {
+        // if the task is not checked off
+        if (! allEntries[i].classList.contains("done") ) {
+            res.push({text: allEntries[i].innerText, priority: allEntries[i].classList });
+        }
+    }
+    localStorage.setItem("todoDatabase",JSON.stringify(res))
+}
+
+restoreList = function(savelist,donefunc) {
+  allTasks = JSON.parse(localStorage.getItem('todoDatabase'))
+  taskList = document.querySelector("#"+savelist);
+  for(i = 0; i < allTasks.length; i++) {
+      taskText = allTasks[i].text;
+      priorityValue = allTasks[i].priority;
+      newli = document.createElement('li');
+
+      /* Original Code
+      for(j = 0; j < priorityValue.length; j++) {
+          newli.classList.add(priorityValue[j]);
+      }
+      */
+
+      /* Start of new Code */
+      newli.classList.add(priorityValue[0]);
+      /* End of new Code */
+
+      newcb = document.createElement('input');
+      newcb.type = 'checkbox';
+      newcb.onclick = donefunc;
+      newli.appendChild(newcb);
+      t = document.createTextNode(taskText);
+      newli.appendChild(t);
+      taskList.appendChild(newli);
+  }
+}
